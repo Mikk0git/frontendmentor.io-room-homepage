1eaf0cfa6b35716fbc406c040d425fe73f7d4f56
Mikk0git <kasprzakmilan1@gmail.com>
2024-02-09 20:51:41 +0100

    shop now arrow

--- src/components/hero/HeroText.tsx
+++ src/components/hero/HeroText.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./Hero.module.css";
 
 interface HeroTextProps {
@@ -5,11 +6,24 @@ interface HeroTextProps {
   text: string;
 }
 const HeroText: React.FC<HeroTextProps> = ({ title, text }) => {
+  const [hover, setHover] = useState(false);
   return (
     <div id={styles.heroText}>
       <h1>{title}</h1>
       <p>{text}</p>
-      <a href="/shop">Shop now</a>
+      <a
+        href="/shop"
+        className={styles.shopLink}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        SHOP NOW{" "}
+        {hover ? (
+          <img src="/icons/icon-arrow-dark-gray.svg" alt="" />
+        ) : (
+          <img src="/icons/icon-arrow-black.svg" alt="" />
+        )}
+      </a>
     </div>
   );
 };
