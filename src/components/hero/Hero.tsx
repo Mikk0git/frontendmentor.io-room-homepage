7e96fc43e77df745f4cce930d5438df9abaccbf2
Mikk0git <kasprzakmilan1@gmail.com>
2024-02-09 20:17:44 +0100

    changing hero text on scroll

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -20,6 +20,7 @@ export default function Hero() {
     "/images/desktop-image-hero-2.jpg",
     "/images/desktop-image-hero-3.jpg",
   ];
+
   const imageSources =
     window.innerWidth > 750 ? imageSourcesDesktop : imageSourcesMobile;
 
@@ -32,10 +33,25 @@ export default function Hero() {
   };
 
   useEffect(() => {
-    const intervalId = setInterval(handleNextImage, 5000);
+    const intervalId = setInterval(handleNextImage, 7500);
 
     return () => clearInterval(intervalId);
   }, [currentImageIndex]);
+
+  const heroText = [
+    {
+      title: "Discover innovative ways to decorate",
+      text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    },
+    {
+      title: "We are available all across the globe",
+      text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+    },
+    {
+      title: "Manufactured with the best materials",
+      text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+    },
+  ];
 
   return (
     <>
@@ -45,8 +61,8 @@ export default function Hero() {
       />
 
       <HeroText
-        title="Discover innovative ways to decorate"
-        text="We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+        title={heroText[currentImageIndex].title}
+        text={heroText[currentImageIndex].text}
       />
       <NavigationButtons
         onNext={handleNextImage}
