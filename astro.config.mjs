22cb0a0876ee2fb1f3dd662b6df8be245f693697
Mikk0git <kasprzakmilan1@gmail.com>
2024-02-09 23:11:20 +0100

    astro config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: "https://mikk0git.github.io",
+  base: "/frontendmentor.io-room-homepage",
 });
